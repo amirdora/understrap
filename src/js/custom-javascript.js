c0b7ca1f9c4c729e663dda99978252c7f68171bf
atikhasan <ah@netzlab.de>
2022-08-23 15:23:49 +0200

    added accordion styling from stackable

--- src/js/custom-javascript.js
+++ src/js/custom-javascript.js
@@ -1,16 +1,1 @@
 // Add your JS customizations here
-$(".webinar-grid .show-more a").on("click", function() {
-	var $this = $(this);
-	var $content = $this.parent().parent().prev("div.wp-block-post-content");
-	var linkText = $this.text().toUpperCase();
-
-	if(linkText === "SHOW MORE"){
-		linkText = "Show less";
-		$content.switchClass("hideContent", "showContent", 400);
-	} else {
-		linkText = "Show more";
-		$content.switchClass("showContent", "hideContent", 400);
-	}
-
-	$this.text(linkText);
-});
